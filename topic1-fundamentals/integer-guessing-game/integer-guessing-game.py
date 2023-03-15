c8d0cae4947b1bc1f76dad44ff4ffa3d59a7743d
djgraves94 <88097664+djgraves94@users.noreply.github.com>
2023-03-15 09:13:12 -0400

    Comment solution with overview and alternate options

--- topic1-fundamentals/integer-guessing-game/integer-guessing-game.py
+++ topic1-fundamentals/integer-guessing-game/integer-guessing-game.py
@@ -1,7 +1,12 @@
+"""
+Though there are many different ways to accomplish te task
+of this assignment, student work should generally look as
+follows.
+"""
+
 from random import randrange
 
 chosenNumber = randrange(1,101)
-print(chosenNumber)
 
 # start game
 print("I thought of a number between 1 and 100! Try to guess it.\n")
@@ -21,10 +26,11 @@ while remainingGuesses > 0:
     else:
         if guess > chosenNumber:
             messageEnd = "less than your guess."
-            upper = guess - 1
+            upper = guess - 1 # (1)
+        # already checked > and ==, so < is the only remaining case
         else:
             messageEnd = "greater than your guess."
-            lower = guess + 1
+            lower = guess + 1 # (1)
         message = "My number is %s"%(messageEnd)
         print(message)
     
@@ -35,3 +41,17 @@ if win:
     print("Correct! You guessed my number in %d guesses. Well done!" %(5 - remainingGuesses + 1))
 else:
     print("Out of guesses! You lose. My number was %d." %(chosenNumber))
+
+
+"""
+(1) As written, this code changes the bounds regardless of
+    whether the guess was inside them. It's
+    possible, for example, for the player to go from [1,49]
+    to [1,76]. To prevent this behavior, students could
+    check whether the guess is within the bounds, and only
+    update the bounds if so.
+
+    More ambitious students could 'force' the user to enter
+    a number within the bounds by using a while loop, only
+    proceeding once a desired number is found.
+"""
