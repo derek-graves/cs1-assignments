c03e477f296eb8be789af27626384cf19bd0f50c
djgraves94 <88097664+djgraves94@users.noreply.github.com>
2023-03-17 09:10:08 -0400

    Include solution without comments

--- topic3-iterables/pin-authentication/pin-authentication.py
+++ topic3-iterables/pin-authentication/pin-authentication.py
@@ -1,1 +1,54 @@
-pass
+from random import randint
+
+def printFormattedNumbers(numType, listOfNums):
+  print(numType + ":   ", end = "")
+  for i in range(len(listOfNums)):
+    # maintain consistent spacing and stay on same line
+    # until the last element in the list 
+    if i == len(listOfNums) - 1:
+      print(listOfNums[i])
+    else:
+      print(listOfNums[i], end = "  ")
+
+def generateCipher(choices):
+  outputCipher = []
+  for i in range(0, 10):
+    digit = randint(1,choices)
+    outputCipher.append(digit)
+  
+  return outputCipher
+
+def encryptPIN(pin, cipher):
+  encrypted = ""
+  for char in pin:
+    char = int(char)
+    encryptedChar = str(cipher[char])
+    encrypted += encryptedChar
+
+  return encrypted
+
+def checkPIN(pin1, pin2):
+  if pin1 == pin2:
+    return True
+  else:
+    return False
+
+
+def main():
+  VALID_DIGITS = [0,1,2,3,4,5,6,7,8,9]
+  TEST_PIN = 75309
+
+  cipher = generateCipher(3)
+  printFormattedNumbers("PIN", VALID_DIGITS)
+  printFormattedNumbers("NUM", cipher)
+  encrypted = encryptPIN(TEST_PIN, cipher)
+
+  enteredPIN = input("Please enter your PIN according to the cipher above.\n")
+  correct = checkPIN(encrypted, enteredPIN)
+
+  if correct:
+    print("Correct! Access granted.")
+  else:
+    print("Incorrect. Access denied.")
+
+main()
