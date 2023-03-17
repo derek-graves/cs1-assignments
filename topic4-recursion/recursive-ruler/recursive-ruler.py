c3751ab400ce77b630376680a854f08aa9dfb08d
djgraves94 <88097664+djgraves94@users.noreply.github.com>
2023-03-17 14:34:09 -0400

    Write solution

--- topic4-recursion/recursive-ruler/recursive-ruler.py
+++ topic4-recursion/recursive-ruler/recursive-ruler.py
@@ -1,1 +1,19 @@
-pass
+def recursiveRuler(n):
+    # base case
+    if n == 1:
+        print("-")
+    # recursive step
+    else:
+        recursiveRuler(n - 1)
+        print(n * "-")
+        recursiveRuler(n - 1)
+
+
+def main():
+    print("\n")
+    size = int(input("What size ruler whould you like? "))
+    print("\n")
+    recursiveRuler(size)
+    print("\n")
+
+main()
