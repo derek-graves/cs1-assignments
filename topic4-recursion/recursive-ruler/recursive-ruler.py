c4b097f8f3443cf48c6c8e610817e7c04267e377
djgraves94 <88097664+djgraves94@users.noreply.github.com>
2023-03-17 14:40:02 -0400

    Comment solution

--- topic4-recursion/recursive-ruler/recursive-ruler.py
+++ topic4-recursion/recursive-ruler/recursive-ruler.py
@@ -1,3 +1,11 @@
+"""
+Student solutions should look almost identical to the one
+below. The key realization is a ruler of size n is actually
+just two rulers of size n - 1 surrounding a line of n marks.
+Once students see that, the implementation should be fairly
+straightforward.
+"""
+
 def recursiveRuler(n):
     # base case
     if n == 1:
